23d0c63885d7928d850065ddb61e413016319ed8
Bruno Malheiros <bruno.malheiros@gmail.com>
2026-04-08 21:33:15 -0300

    aula6

---
+++ ex26.py
@@ -0,0 +1,7 @@
+#Desenvolva um codigo que
+#some os valores automaticamente
+#entre 1 e 10
+soma = 0
+for i in range(1,11):
+    soma = soma + i #variavel cumulativa
+    print(f"{soma}") #pode ssr print(soma) tambem #se o print estiver fora do for mostrara so o resultado final
